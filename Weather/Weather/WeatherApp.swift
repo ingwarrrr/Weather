f55d5ca0d50e1844383c84032f75bdbd952075f3
ingwarrrr <igorek0706@gmail.com>
2022-10-16 04:55:22 +0300

    Initial commit

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Igor on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
