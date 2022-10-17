596989edf3e82bd027e6c310030e9dd329a932c2
ingwarrrr <igorek0706@gmail.com>
2022-10-17 07:35:21 +0300

    WeatherView Updated

--- Weather/Weather/View/WeatherView.swift
+++ Weather/Weather/View/WeatherView.swift
@@ -22,9 +22,57 @@ struct WeatherView: View {
                 .frame(maxWidth: .infinity, alignment: .leading)
                 
                 Spacer()
+                
+                VStack {
+                    HStack {
+                        VStack(spacing: 20) {
+                            Image(systemName: "sun.max")
+                                .font(.system(size: 40))
+                            
+                            Text(weather.weather[0].main)
+                        }.frame(width: 150, alignment: .leading)
+                        
+                        Spacer()
+                        
+                        Text(weather.main.feelsLike.roundDouble() + "°")
+                            .font(.system(size: 100))
+                            .fontWeight(.bold)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                        .frame(height: 40)
+                    
+                    AsyncImage(url: URL(string: "https://static.vecteezy.com/system/resources/previews/000/183/639/large_2x/flat-design-vector-landscape-illustration.jpg")) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                            .cornerRadius(35)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
             }
             .padding()
             .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
         }
         .edgesIgnoringSafeArea(.bottom)
         .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
